6ab655c4b04ebecb49f4dfd0a703560e90aa6599
mwezn <m.w.g.nelson@gmail.com>
2021-03-01 09:12:15 +0000

    Added dispatch function

--- src/containers2/NavBar2.js
+++ src/containers2/NavBar2.js
@@ -1,16 +1,20 @@
 
-//import { AuthContext } from '../App2'
-const My_Token= localStorage.getItem('token');
-console.log(My_Token)
+import { AuthContext } from '../App2'
+import {useContext } from "react";
+import './App2.css'
+
+
 
 
 
 function Navigate(props){
+  const  { dispatch } = useContext(AuthContext);
+  
   return props.authed?(
     <div>
       <ul className="NavBar">
         <li><a href="/" onClick={(e)=>e.preventDefault()}>Home</a></li>
-        <li><a href="/logout" onClick={()=>localStorage.removeItem('token')}>Logout</a></li>
+        <li><a href="/logout" onClick={()=>dispatch({type: "LOGOUT"})}>Logout</a></li>
       </ul>
     </div>):(
       <div>
